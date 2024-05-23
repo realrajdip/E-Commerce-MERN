e30b8d8b6fa7a63f44f5ebaa7408d577bda10864
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-05-23 22:39:35 +0530

    mongoose updated

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const app = express(); 
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const ownersRoute = require('./routes/ownersRouter.js')
+const usersRoute = require('./routes/usersRoute.js'); 
+const productsRouter = require('./routes/productsRoute.js') 
 
-const cookieParser = require('cookie-parser');
-const path = require('path')
+const db = require("./config/mongoose-connection.js");
 
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.send('hi')
-}); 
+app.use("/owners", ownersRoute);
+app.use("/users", usersRoute);
+app.use("/products", productsRouter);
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000')
-}); 
+  console.log("server is running on port 3000");
+});
