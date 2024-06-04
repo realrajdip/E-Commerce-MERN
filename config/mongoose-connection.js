d4ff4e49422f1bc3c3eeed32ca4842997365d204
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-06-04 23:48:56 +0530

    updated

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-const dbgr = require('debug')('development:mongoos')
+// const mongoose = require('mongoose');
+// const dbgr = require('debug')('development:mongoos')
 
-mongoose
-.connect("mongodb://127.0.0.1:27017/scatch")
-.then(() => console.log("connected"))
-.catch((err) => console.log(err))
+// mongoose
+// .connect("mongodb://127.0.0.1:27017/scatch")
+// .then(() => console.log("connected"))
+// .catch((err) => console.log(err))
 
-module.exports = mongoose.connection;  
+// module.exports = mongoose.connection;  
