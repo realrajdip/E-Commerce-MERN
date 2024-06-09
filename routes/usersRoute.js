db8901b5bbb68767f3075c7a49bc9016b5d0fff6
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-06-09 23:52:40 +0530

    product schema is updated

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,8 +1,33 @@
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
+const userModel = require("../models/user-model");
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-router.get('/', (req, res) => {
-    res.send("userrou")
-})
+router.get("/", (req, res) => {
+  res.send("userrou");
+});
 
-module.exports = router; 
+router.post("/register", async function (req, res) {
+  try {
+    const { fullname, email, password } = req.body;
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await userModel.create({
+      email,
+      password: hashedPassword,
+      fullname,
+    });
+    
+    let token = jwt.sign({email, id: user._id}, "secret"); 
+    
+    res.cookie("token", token); 
+    res.send("user created successfully"); 
+    
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+module.exports = router;
